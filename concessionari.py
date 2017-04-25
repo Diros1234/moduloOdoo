32a0d62a70eb35b045f4a50cfbdf005daaea5f4c
DanielV <diro_bdn89@hotmail.com>
2017-04-25 15:44:33 +0200

    calendar funcionando

--- concessionari.py
+++ concessionari.py
@@ -12,27 +12,23 @@ class concessionari_revisio(models.Model):
 	_name='concessionari.revisio'
 	data_revisio=fields.Date('Data revisio')
 	#Make table 'Revisions' that contains primary key of 'class mecanic' and 'class cotxe'
-	mecanic=fields.Many2one('concessionari.mecanic','Revisions',ondelete='cascade')
-	cotxe=fields.Many2one('concessionari.cotxe','Revisions',ondelete='cascade')
-	_rec_name='data_revisio'
+	mecanic=fields.Many2one('concessionari.mecanic','Mecanics',ondelete='cascade')
+	cotxe=fields.Many2one('concessionari.cotxe','Cotxes',ondelete='cascade')
 class concessionari_cotxe(models.Model):
 	_name='concessionari.cotxe'
 	matricula = fields.Char('Matricula',size=7)
 	marca = fields.Char('Marca',size=20)
 	model = fields.Char('Modelo',size=20)
-	nou = fields.Boolean('Nou?')
+	nou = fields.Boolean('Nou')
 	preu = fields.Float('Preu')
 	km = fields.Integer('km')
-	def devolverMatricula(self):
-		return 'Matricula : '+matricula+' Marca : '+marca+ ' Modelo : '+model
-	cotxeConcat = fields.Char('cotxeConcat',compute='devolverMatricula')
-	_rec_name = 'cotxeConcat'
+	_rec_name = 'matricula'
 class concessionari_compra(models.Model):
 	_name = 'concessionari.compra'
 	client = fields.Many2one('concessionari.client','Clientes',ondelete='cascade')
 	cotxe  = fields.Many2one('concessionari.cotxe','Cotxes',ondelete='cascade')
-	data=fields.Date('Fecha de compra')
-	_rec_name='data'
+	data=fields.Datetime('Fecha de compra')
+	frase=fields.Char('identificadores')
 class concessionari_client(models.Model):
 	_name = 'concessionari.client'
 	codi = fields.Integer('Codi')
@@ -40,9 +36,11 @@ class concessionari_client(models.Model):
 	cognoms =fields.Char('Cognom',size=50)
 	telefon = fields.Char('Telefon',size=9)
 	venedor =fields.Many2one('concessionari.venedor','Venedors',ondelete='cascade')
+	_rec_name='codi'
 class concessionari_venedor(models.Model):
 	_name = 'concessionari.venedor'
 	codi = fields.Integer('Codi')
 	nom = fields.Char('Nom',size=30)
 	cognoms = fields.Char('Cognom',size=50)
 	telefon = fields.Char('Telefon',size=9)
+	_rec_name='codi'
